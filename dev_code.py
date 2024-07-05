5dcbcd8a09af47adb3eb7ab182c1d0c60e214242
Sreeni <kattubadi426@gmail.com>
2024-07-05 08:02:54 +0530

    update from dell

--- dev_code.py
+++ dev_code.py
@@ -1,168 +1,168 @@
 print("this is dev code")
-# import datetime
-# import json
-# import os
-# import sys
-# import pandas as pd
-# from pyspark.sql.functions import explode_outer, concat, col, \
-#     trim,to_date, lpad, lit, count,max, min, explode, current_timestamp
-# from pyspark.sql import SparkSession
-# import getpass
-# from utility.general_utility import fetch_file_path
-# os.environ.setdefault("project_path", os.getcwd())
-# project_path = os.environ.get("project_path")
+import datetime
+import json
+import os
+import sys
+import pandas as pd
+from pyspark.sql.functions import explode_outer, concat, col, \
+    trim,to_date, lpad, lit, count,max, min, explode, current_timestamp
+from pyspark.sql import SparkSession
+import getpass
+from utility.general_utility import fetch_file_path
+os.environ.setdefault("project_path", os.getcwd())
+project_path = os.environ.get("project_path")
+
+
+# jar_path = pkg_resources.resource_filename('jars', 'postgresql-42.2.5.jar')
+#postgre_jar = project_path + "/jars/postgresql-42.2.5.jar"
+snow_jar = project_path + "/jars/snowflake-jdbc-3.14.3.jar"
+#oracle_jar = project_path + "/jars/ojdbc11.jar"
+
+jar_path = snow_jar
+spark = SparkSession.builder.master("local[1]") \
+    .appName("test") \
+    .config("spark.jars", jar_path) \
+    .config("spark.driver.extraClassPath", jar_path) \
+    .config("spark.executor.extraClassPath", jar_path) \
+    .getOrCreate()
+
+
+# Fetch system user's login name
+system_user = getpass.getuser()
+
+batch_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+
+
+# Load from File to DB raw table
+
+file_path = fetch_file_path('contact_info_20240703.csv')
+
+file = spark.read.csv(file_path, header=True, inferSchema=True)
+file = file.filter(file.Identifier.isNotNull())
+
+file.show()
+
+
+
+file= file.withColumn('batch_date', lit(batch_id))\
+    .withColumn('create_date', current_timestamp())\
+    .withColumn('update_date', current_timestamp())\
+    .withColumn('create_user',lit(system_user))\
+    .withColumn('update_user',lit(system_user))
+
+file.show()
+
+url = 'jdbc:snowflake://oborokf-kh65378.snowflakecomputing.com/?user=KSREENIVASULU&password=Dharmavaram1@&warehouse=COMPUTE_WH&db=SAMPLE&schema=CONTACT_INFO'
+
+file.write.mode("overwrite") \
+    .format("jdbc") \
+    .option("driver", "net.snowflake.client.jdbc.SnowflakeDriver") \
+    .option("url", url) \
+    .option("dbtable", "ETL_AUTO.CONTACT_INFO.CONTACT_INFO_RAW") \
+    .save()
+
+
+file.createOrReplaceTempView("file")
+
+contact_info_bronze = spark.sql(
+    """ select
+    cast(Identifier as decimal(10)) Identifier,
+    upper(Surname) Surname,
+    upper(given_name) given_name,
+    middle_initial,
+    suffix,
+    Primary_street_number,
+    primary_street_name,
+    upper(city) city,
+    state,
+    zipcode,
+    Primary_street_number_prev,
+    primary_street_name_prev,
+    city_prev,
+    state_prev,
+    zipcode_prev,
+    Email,
+    translate(Phone,'+-','') phone,
+    rpad(birthmonth,8,'0') birthmonth
+    from file
+    """
+ )
+
+contact_info_bronze= contact_info_bronze.withColumn('batch_date', lit(batch_id))\
+    .withColumn('create_date', current_timestamp())\
+    .withColumn('update_date', current_timestamp())\
+    .withColumn('create_user',lit(system_user))\
+    .withColumn('update_user',lit(system_user))
+
+
+contact_info_bronze.write.mode("overwrite") \
+    .format("jdbc") \
+    .option("driver", "net.snowflake.client.jdbc.SnowflakeDriver") \
+    .option("url", url) \
+    .option("dbtable", "ETL_AUTO.CONTACT_INFO.CONTACT_INFO_BRONZE") \
+    .save()
+
+
+contact_info_bronze.createOrReplaceTempView("contact_info_bronze")
+
+contact_info_silver= spark.sql(
+        """
+        select
+        Identifier,
+        Surname,
+        given_name,
+        middle_initial,
+        Primary_street_number,
+        primary_street_name,
+        city,
+        state,
+        zipcode,
+        Email,
+        Phone,
+        birthmonth,
+        'Y' as Current_ind
+        from contact_info_bronze
+        union
+        select
+        Identifier,
+        Surname,
+        given_name,
+        middle_initial,
+        Primary_street_number_prev,
+        primary_street_name_prev,
+        city_prev,
+        state_prev,
+        zipcode_prev,
+        Email,
+        Phone,
+        birthmonth,
+        'N' as Current_ind
+        from contact_info_bronze
+        """)
+
+contact_info_silver=contact_info_silver.withColumn('batch_date', lit(batch_id))\
+    .withColumn('create_date', current_timestamp())\
+    .withColumn('update_date', current_timestamp())\
+    .withColumn('create_user',lit(system_user))\
+    .withColumn('update_user',lit(system_user))
+
+
+
+
+# #load data to snowflake
 #
-#
-# # jar_path = pkg_resources.resource_filename('jars', 'postgresql-42.2.5.jar')
-# #postgre_jar = project_path + "/jars/postgresql-42.2.5.jar"
-# snow_jar = project_path + "/jars/snowflake-jdbc-3.14.3.jar"
-# #oracle_jar = project_path + "/jars/ojdbc11.jar"
-#
-# jar_path = snow_jar
-# spark = SparkSession.builder.master("local[1]") \
-#     .appName("test") \
-#     .config("spark.jars", jar_path) \
-#     .config("spark.driver.extraClassPath", jar_path) \
-#     .config("spark.executor.extraClassPath", jar_path) \
-#     .getOrCreate()
-#
-#
-# # Fetch system user's login name
-# system_user = getpass.getuser()
-#
-# batch_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-#
-#
-#
-# # Load from File to DB raw table
-#
-# file_path = fetch_file_path('contact_info_20240703.csv')
-#
-# file = spark.read.csv(file_path, header=True, inferSchema=True)
-# file = file.filter(file.Identifier.isNotNull())
-#
-# file.show()
-#
-#
-#
-# file= file.withColumn('batch_date', lit(batch_id))\
-#     .withColumn('create_date', current_timestamp())\
-#     .withColumn('update_date', current_timestamp())\
-#     .withColumn('create_user',lit(system_user))\
-#     .withColumn('update_user',lit(system_user))
-#
-# file.show()
-#
-# url = 'jdbc:snowflake://oborokf-kh65378.snowflakecomputing.com/?user=KSREENIVASULU&password=Dharmavaram1@&warehouse=COMPUTE_WH&db=SAMPLE&schema=CONTACT_INFO'
-#
-# file.write.mode("overwrite") \
-#     .format("jdbc") \
-#     .option("driver", "net.snowflake.client.jdbc.SnowflakeDriver") \
-#     .option("url", url) \
-#     .option("dbtable", "ETL_AUTO.CONTACT_INFO.CONTACT_INFO_RAW") \
-#     .save()
-#
-#
-# file.createOrReplaceTempView("file")
-#
-# contact_info_bronze = spark.sql(
-#     """ select
-#     cast(Identifier as decimal(10)) Identifier,
-#     upper(Surname) Surname,
-#     upper(given_name) given_name,
-#     middle_initial,
-#     suffix,
-#     Primary_street_number,
-#     primary_street_name,
-#     upper(city) city,
-#     state,
-#     zipcode,
-#     Primary_street_number_prev,
-#     primary_street_name_prev,
-#     city_prev,
-#     state_prev,
-#     zipcode_prev,
-#     Email,
-#     translate(Phone,'+-','') phone,
-#     rpad(birthmonth,8,'0') birthmonth
-#     from file
-#     """
-#  )
-#
-# contact_info_bronze= contact_info_bronze.withColumn('batch_date', lit(batch_id))\
-#     .withColumn('create_date', current_timestamp())\
-#     .withColumn('update_date', current_timestamp())\
-#     .withColumn('create_user',lit(system_user))\
-#     .withColumn('update_user',lit(system_user))
-#
-#
-# contact_info_bronze.write.mode("overwrite") \
-#     .format("jdbc") \
-#     .option("driver", "net.snowflake.client.jdbc.SnowflakeDriver") \
-#     .option("url", url) \
-#     .option("dbtable", "ETL_AUTO.CONTACT_INFO.CONTACT_INFO_BRONZE") \
-#     .save()
-#
-#
-# contact_info_bronze.createOrReplaceTempView("contact_info_bronze")
-#
-# contact_info_silver= spark.sql(
-#         """
-#         select
-#         Identifier,
-#         Surname,
-#         given_name,
-#         middle_initial,
-#         Primary_street_number,
-#         primary_street_name,
-#         city,
-#         state,
-#         zipcode,
-#         Email,
-#         Phone,
-#         birthmonth,
-#         'Y' as Current_ind
-#         from contact_info_bronze
-#         union
-#         select
-#         Identifier,
-#         Surname,
-#         given_name,
-#         middle_initial,
-#         Primary_street_number_prev,
-#         primary_street_name_prev,
-#         city_prev,
-#         state_prev,
-#         zipcode_prev,
-#         Email,
-#         Phone,
-#         birthmonth,
-#         'N' as Current_ind
-#         from contact_info_bronze
-#         """)
-#
-# contact_info_silver=contact_info_silver.withColumn('batch_date', lit(batch_id))\
-#     .withColumn('create_date', current_timestamp())\
-#     .withColumn('update_date', current_timestamp())\
-#     .withColumn('create_user',lit(system_user))\
-#     .withColumn('update_user',lit(system_user))
-#
-#
-#
-#
-# # #load data to snowflake
-# #
-#
-#
-#
-#
-#
-# contact_info_silver.write.mode("overwrite") \
-#     .format("jdbc") \
-#     .option("driver", "net.snowflake.client.jdbc.SnowflakeDriver") \
-#     .option("url", url) \
-#     .option("dbtable", "ETL_AUTO.CONTACT_INFO.CONTACT_INFO_SILVER") \
-#     .save()
-#
-# spark.stop()
-#
+
+
+
+
+
+contact_info_silver.write.mode("overwrite") \
+    .format("jdbc") \
+    .option("driver", "net.snowflake.client.jdbc.SnowflakeDriver") \
+    .option("url", url) \
+    .option("dbtable", "ETL_AUTO.CONTACT_INFO.CONTACT_INFO_SILVER") \
+    .save()
+
+spark.stop()
+
--- main.py
+++ main.py
@@ -1,135 +1,135 @@
 print("execution started in github")
-# import pandas as pd
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import collect_set
-# import os
-#
-# from utility.read_data import read_file, read_db, read_snowflake
-# from utility.validation_library import count_check, duplicate_check, records_present_only_in_source, \
-#     records_present_only_in_target,data_compare,schema_check,null_value_check,uniqueness_check,column_value_reference_check,column_range_check,name_check
-#
-# project_path = os.getcwd()
-#
-# postgre_jar = project_path + "/jars/postgresql-42.2.5.jar"
-#
-# snow_jar = project_path + "/jars/snowflake-jdbc-3.14.3.jar"
-# #oracle_jar = project_path + "/jars/ojdbc11.jar"
-#
-# jar_path = snow_jar
-#
-# spark = SparkSession.builder.master("local[5]") \
-#     .appName("test") \
-#     .config("spark.jars", jar_path) \
-#     .config("spark.driver.extraClassPath", jar_path) \
-#     .config("spark.executor.extraClassPath", jar_path) \
-#     .getOrCreate()
-#
-# Out = {
-#     "validation_Type": [],
-#     "Source_name": [],
-#     "target_name": [],
-#     "Number_of_source_Records": [],
-#     "Number_of_target_Records": [],
-#     "Number_of_failed_Records": [],
-#     "column": [],
-#     "Status": [],
-#     "source_type": [],
-#     "target_type": []
-# }
-#
-# template_path = project_path + '\config\master_test_template_project1.xlsx'
-#
-# test_cases = pd.read_excel(template_path)
-#
-# print(test_cases)
-#
-# run_test_case = test_cases.loc[(test_cases.execution_ind == 'Y')]
-#
-# run_test_case = spark.createDataFrame(run_test_case)
-#
-# validation = (run_test_case.groupBy('source', 'source_type',
-#                                     'source_db_name', 'source_schema_path', 'source_transformation_query_path',
-#                                     'target', 'target_type', 'target_db_name', 'target_schema_path',
-#                                     'target_transformation_query_path',
-#                                     'key_col_list', 'null_col_list', 'exclude_columns',
-#                                     'unique_col_list', 'dq_column', 'expected_values', 'min_val', 'max_val').
-#               agg(collect_set('validation_Type').alias('validation_Type')))
-#
-# validation.show(truncate=False)
-#
-# validations = validation.collect()
-#
-# print(validations)
-#
-# for row in validations:
-#     print("row", row)
-#     if row['source_type'] == 'table':
-#         source = read_db(spark=spark,
-#                          table=row['source'],
-#                          database=row['source_db_name'],
-#                          query_path=row['source_transformation_query_path'],
-#                          row=row)
-#     elif row['source_type'] == 'snowflake':
-#         source = read_snowflake(spark=spark,
-#                         table=row['source'],
-#                         database=row['source_db_name'],
-#                         query=row['source_transformation_query_path'], row=row)
-#
-#
-#     else:
-#         source = read_file(type=row['source_type'],
-#                            file_name=row['source'],
-#                            spark=spark,
-#                            row=row,
-#                            schema=row['source_schema_path'])
-#
-#     if row['target_type'] == 'table':
-#         target = read_db(spark=spark,
-#                          table=row['target'],
-#                          database=row['target_db_name'],
-#                          query_path=row['target_transformation_query_path'],
-#                          row=row)
-#     elif row['target_type'] == 'snowflake':
-#         target = read_snowflake(spark=spark,
-#                         table=row['target'],
-#                         database=row['target_db_name'],
-#                         query=row['target_transformation_query_path'], row=row)
-#     else:
-#         target = read_file(type=row['target_type'],
-#                            file_name=row['target'],
-#                            spark=spark,
-#                            row=row,
-#                            schema=row['target_schema_path'])
-#     source.show()
-#     target.show()
-#     print(row['validation_Type'])
-#     for validation in row['validation_Type']:
-#         print(validation)
-#         if validation == 'count_check':
-#             count_check(source, target,row,Out)
-#         elif validation == 'records_present_only_in_source':
-#             records_present_only_in_source(source, target, row['key_col_list'], Out, row)
-#         elif validation == 'records_present_only_target':
-#             records_present_only_in_target(source, target, row['key_col_list'], Out, row)
-#         elif validation == 'duplicate':
-#             duplicate_check(target,row['key_col_list'],row,Out)
-#         elif validation == 'data_compare':
-#             data_compare(source, target, row['key_col_list'], Out, row)
-#         elif validation == 'schema_check':
-#             schema_check(source, target, spark, Out, row)
-#             #call schema check function
-#         elif validation == 'null_check':
-#             null_value_check(target, row['null_col_list'], Out, row)
-#         elif validation == 'uniqueness_check':
-#             uniqueness_check(target,row['unique_col_list'],Out, row)
-#         else:
-#             print("validation is not defined")
-#
-#
-# print(Out)
-#
-# summary = pd.DataFrame(Out)
-#
-# print(summary)
-#
-# summary.to_csv("summary.csv")
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import collect_set
+import os
+
+from utility.read_data import read_file, read_db, read_snowflake
+from utility.validation_library import count_check, duplicate_check, records_present_only_in_source, \
+    records_present_only_in_target,data_compare,schema_check,null_value_check,uniqueness_check,column_value_reference_check,column_range_check,name_check
+
+project_path = os.getcwd()
+
+postgre_jar = project_path + "/jars/postgresql-42.2.5.jar"
+
+snow_jar = project_path + "/jars/snowflake-jdbc-3.14.3.jar"
+#oracle_jar = project_path + "/jars/ojdbc11.jar"
+
+jar_path = snow_jar
+
+spark = SparkSession.builder.master("local[5]") \
+    .appName("test") \
+    .config("spark.jars", jar_path) \
+    .config("spark.driver.extraClassPath", jar_path) \
+    .config("spark.executor.extraClassPath", jar_path) \
+    .getOrCreate()
+
+Out = {
+    "validation_Type": [],
+    "Source_name": [],
+    "target_name": [],
+    "Number_of_source_Records": [],
+    "Number_of_target_Records": [],
+    "Number_of_failed_Records": [],
+    "column": [],
+    "Status": [],
+    "source_type": [],
+    "target_type": []
+}
+
+template_path = project_path + '\config\master_test_template_project1.xlsx'
+
+test_cases = pd.read_excel(template_path)
+
+print(test_cases)
+
+run_test_case = test_cases.loc[(test_cases.execution_ind == 'Y')]
+
+run_test_case = spark.createDataFrame(run_test_case)
+
+validation = (run_test_case.groupBy('source', 'source_type',
+                                    'source_db_name', 'source_schema_path', 'source_transformation_query_path',
+                                    'target', 'target_type', 'target_db_name', 'target_schema_path',
+                                    'target_transformation_query_path',
+                                    'key_col_list', 'null_col_list', 'exclude_columns',
+                                    'unique_col_list', 'dq_column', 'expected_values', 'min_val', 'max_val').
+              agg(collect_set('validation_Type').alias('validation_Type')))
+
+validation.show(truncate=False)
+
+validations = validation.collect()
+
+print(validations)
+
+for row in validations:
+    print("row", row)
+    if row['source_type'] == 'table':
+        source = read_db(spark=spark,
+                         table=row['source'],
+                         database=row['source_db_name'],
+                         query_path=row['source_transformation_query_path'],
+                         row=row)
+    elif row['source_type'] == 'snowflake':
+        source = read_snowflake(spark=spark,
+                        table=row['source'],
+                        database=row['source_db_name'],
+                        query=row['source_transformation_query_path'], row=row)
+
+
+    else:
+        source = read_file(type=row['source_type'],
+                           file_name=row['source'],
+                           spark=spark,
+                           row=row,
+                           schema=row['source_schema_path'])
+
+    if row['target_type'] == 'table':
+        target = read_db(spark=spark,
+                         table=row['target'],
+                         database=row['target_db_name'],
+                         query_path=row['target_transformation_query_path'],
+                         row=row)
+    elif row['target_type'] == 'snowflake':
+        target = read_snowflake(spark=spark,
+                        table=row['target'],
+                        database=row['target_db_name'],
+                        query=row['target_transformation_query_path'], row=row)
+    else:
+        target = read_file(type=row['target_type'],
+                           file_name=row['target'],
+                           spark=spark,
+                           row=row,
+                           schema=row['target_schema_path'])
+    source.show()
+    target.show()
+    print(row['validation_Type'])
+    for validation in row['validation_Type']:
+        print(validation)
+        if validation == 'count_check':
+            count_check(source, target,row,Out)
+        elif validation == 'records_present_only_in_source':
+            records_present_only_in_source(source, target, row['key_col_list'], Out, row)
+        elif validation == 'records_present_only_target':
+            records_present_only_in_target(source, target, row['key_col_list'], Out, row)
+        elif validation == 'duplicate':
+            duplicate_check(target,row['key_col_list'],row,Out)
+        elif validation == 'data_compare':
+            data_compare(source, target, row['key_col_list'], Out, row)
+        elif validation == 'schema_check':
+            schema_check(source, target, spark, Out, row)
+            #call schema check function
+        elif validation == 'null_check':
+            null_value_check(target, row['null_col_list'], Out, row)
+        elif validation == 'uniqueness_check':
+            uniqueness_check(target,row['unique_col_list'],Out, row)
+        else:
+            print("validation is not defined")
+
+
+print(Out)
+
+summary = pd.DataFrame(Out)
+
+print(summary)
+
+summary.to_csv("summary.csv")

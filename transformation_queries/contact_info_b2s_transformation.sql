6a1d44fae2d11240734df138d684cadbef9f982d
Sreeni <kattubadi426@gmail.com>
2024-07-05 07:37:31 +0530

    update from dell

---
+++ transformation_queries/contact_info_b2s_transformation.sql
@@ -0,0 +1,43 @@
+SELECT
+    "Identifier",
+    "Surname",
+    "given_name",
+    "middle_initial",
+    "Primary_street_number",
+    "primary_street_name",
+    "city",
+    "state",
+    "zipcode",
+    "Email",
+     "phone"  ,
+    "birthmonth",
+    'Y' as "Current_ind",
+    "batch_date",
+    "create_date",
+    "update_date",
+    "create_user",
+    "update_user"
+FROM
+     ETL_AUTO.CONTACT_INFO.contact_info_bronze
+union all
+SELECT
+    "Identifier",
+    "Surname",
+    "given_name",
+    "middle_initial",
+    "Primary_street_number_prev",
+    "primary_street_name_prev",
+    "city_prev",
+    "state_prev",
+    "zipcode_prev",
+    "Email",
+     "phone"  ,
+    "birthmonth",
+    'N' as "Current_ind",
+    "batch_date",
+    "create_date",
+    "update_date",
+    "create_user",
+    "update_user"
+FROM
+     ETL_AUTO.CONTACT_INFO.contact_info_bronze
